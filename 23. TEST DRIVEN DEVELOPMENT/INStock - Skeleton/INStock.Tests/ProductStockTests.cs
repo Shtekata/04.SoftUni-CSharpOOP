f6ff4c8adcac02d718d17852f15651388432ec81
Shtekata <gesheval@gmail.com>
2020-02-17 20:32:49 +0200

    23

---
+++ 23. TEST DRIVEN DEVELOPMENT/INStock - Skeleton/INStock.Tests/ProductStockTests.cs
@@ -0,0 +1,128 @@
+﻿namespace INStock.Tests
+{
+    using INStock.Contracts;
+    using NUnit.Framework;
+    using System;
+    using System.Linq;
+
+    [TestFixture]
+    public class ProductStockTests
+    {
+        private ProductStock productStock;
+
+        [SetUp]
+        public void CreateTestObjects()
+        {
+            productStock = new ProductStock();
+
+            productStock.Add(new Product()
+            {
+                Label = "MyProduct",
+                Quantity = 1,
+                Price = 100
+            });
+        }
+
+        [Test]
+        public void DuplicateLabelAfterAddingNewProduct()
+        {
+            var countBeforeAdd = productStock.Count;
+            productStock.Add(new Product() { Label = "MyProduct", Price = 100 });
+
+            Assert.That(productStock.Count == countBeforeAdd);
+        }
+
+        [Test]
+        public void ProductQuantityIncreasedByQuatityAdded()
+        {
+            var quantityBefore = productStock.FirstOrDefault().Quantity;
+            productStock.Add(new Product() { Label = "MyProduct", Quantity = 5, Price = 100 });
+
+            Assert.That(productStock.FirstOrDefault().Quantity == 6);
+        }
+
+        [Test]
+        public void PricePreservedAfterNewProductAdded()
+        {
+            var product = new Product() { Label = "MyProduct", Price = 5.9m };
+
+            Assert.That(() => productStock.Add(product), Throws.ArgumentException);
+        }
+
+        [Test]
+        public void TrueIfContainsProduct()
+        {
+            var product = new Product()
+            {
+                Label = "MyProduct",
+                Quantity = 1,
+                Price = 100
+            };
+
+            Assert.That(productStock.Contains(product));
+        }
+
+        [Test]
+        public void FalseIfContainsProduct()
+        {
+            var product = new Product()
+            {
+                Label = "MyProduct1",
+                Quantity = 1,
+                Price = 100
+            };
+
+            Assert.That(!productStock.Contains(product));
+        }
+
+        [Test]
+        public void FindNthProductInStock()
+        {
+            var product = new Product()
+            {
+                Label = "NewProduct",
+                Quantity = 1,
+                Price = 100
+            };
+
+            productStock.Add(product);
+            var findedProduct = productStock.Find(2);
+
+            Assert.That(findedProduct, Is.EqualTo(product));
+        }
+
+        [Test]
+        public void ErrorIfProductIndexIsNotValid()
+        {
+            Assert.Throws<IndexOutOfRangeException>(() => productStock.Find(8));
+        }
+
+        [Test]
+        public void ProductFoundByLabel()
+        {
+            var product = productStock.FindByLabel("MyProduct");
+
+            Assert.AreEqual(product, productStock.First());
+        }
+
+        [Test]
+        public void ErrorIfLabelNotFound()
+        {
+            Assert.Throws<ArgumentException>(() => productStock.FindByLabel("NewProduct"));
+        }
+
+        [Test]
+        public void EmptyListIfNotFound()
+        {
+            var products = productStock.FindAllInPriceRange(1, 2);
+
+            Assert.That(products.Count() == 0);
+        }
+
+        [TearDown]
+        public void DestroyObjects()
+        {
+            productStock = null;
+        }
+    }
+}
---
+++ 23. TEST DRIVEN DEVELOPMENT/INStock - Skeleton/INStock/ProductStock.cs
@@ -0,0 +1,107 @@
+﻿using INStock.Contracts;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace INStock
+{
+    public class ProductStock : IProductStock
+    {
+        private List<IProduct> products;
+
+        public ProductStock()
+        {
+            products = new List<IProduct>();
+        }
+
+        public IProduct this[int index] { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public int Count { get => products.Count; }
+
+        public void Add(IProduct product)
+        {
+            var existingProduct = (Product)products.FirstOrDefault(x => x.Label == product.Label);//FindByLabel(product.Label);
+
+            if (existingProduct == null)
+            {
+                products.Add(product);
+            }
+            else
+            {
+                if (existingProduct.Price != product.Price)
+                {
+                    throw new ArgumentException();
+                }
+
+                existingProduct.Quantity += product.Quantity;
+            }
+        }
+
+        public bool Contains(IProduct product)
+        {
+            return products.Any(x => x.Label == product.Label);
+        }
+
+        public IProduct Find(int index)
+        {
+            try
+            {
+                return products[index - 1];
+            }
+            catch (ArgumentOutOfRangeException ae)
+            {
+
+                throw new IndexOutOfRangeException(ae.Message, ae);
+            }
+        }
+
+        public IEnumerable<IProduct> FindAllInPriceRange(decimal lo, decimal hi)
+        {
+            return products.Where(x => x.Price >= lo && x.Price <= hi);
+        }
+
+        public IEnumerable<IProduct> FindAllByQuantity(int quantity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<IProduct> FindAllByPrice(double lo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IProduct FindByLabel(string label)
+        {
+            var product = products.FirstOrDefault(x => x.Label == label);
+            
+            if (product == null)
+            {
+                throw new ArgumentException();
+            }
+            
+            return product;
+        }
+
+        public IProduct FindMostExpensiveProduct()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<IProduct> GetEnumerator()
+        {
+            return products.GetEnumerator();
+        }
+
+        public bool Remove(IProduct product)
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
