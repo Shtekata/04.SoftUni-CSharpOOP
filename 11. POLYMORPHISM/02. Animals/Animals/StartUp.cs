98b5e05119cb91e02cd87922a359aef052f38c32
Shtekata <gesheval@gmail.com>
2020-02-17 20:19:09 +0200

    11

---
+++ 11. POLYMORPHISM/02. Animals/Animals/StartUp.cs
@@ -0,0 +1,15 @@
+﻿namespace Animals
+{
+    using System;
+    public class StartUp
+    {
+        public  static void Main(string[] args)
+        {
+            Animal dog = new Dog("Gosho", "Meat");
+            Animal cat = new Cat("Pesho", "Whiskas");
+
+            Console.WriteLine(dog.ExplainSelf());
+            Console.WriteLine(cat.ExplainSelf());
+        }
+    }
+}
---
+++ 11. POLYMORPHISM/03. Shapes/Shapes/Rectangle.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Shapes
+{
+    public class Rectangle : Shape
+    {
+        private double height;
+        private double width;
+        public Rectangle(double height, double width)
+        {
+            Height = height;
+            Width = width;
+        }
+        public double Height { get; private set; }
+        public double Width { get; private set; }
+
+        public override double CalculateArea()
+        {
+            return Height * Width;
+        }
+
+        public override double CalculatePerimeter()
+        {
+            return (Height + Width) * 2;
+        }
+            
+        public override string Draw()
+        {
+            return base.Draw() + this.GetType().Name;
+        }
+    }
+}
