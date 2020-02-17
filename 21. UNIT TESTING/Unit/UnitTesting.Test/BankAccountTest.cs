fb7e5c3f7c1ddd721dcfae9fe02e5d1b015a3a50
Shtekata <gesheval@gmail.com>
2020-02-17 20:29:10 +0200

    21

---
+++ 21. UNIT TESTING/Unit/UnitTesting.Test/BankAccountTest.cs
@@ -0,0 +1,55 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitTesting.Test
+{
+    [TestFixture]
+    public class BankAccountTest
+    {
+        private BankAccount bankAccount;
+        [SetUp]
+        public void CreateBankAccount()
+        {
+            bankAccount = new BankAccount(100m);
+        }
+        [TearDown]
+        public void destroyBankAccount()
+        {
+            bankAccount = null;
+        }
+        [Test]
+        public void TestNewBankAccount()
+        {
+            Assert.That(bankAccount.Balance, Is.EqualTo(100m), "Creating a new Bank Account");
+        }
+        [Test]
+        public void TestNewBankAccountWithNegativeBalance()
+        {
+            Assert.That(() => new BankAccount(-100m), Throws.ArgumentException.With.Message.EqualTo("Balance can not be negative!"));
+        }
+        [Test ]
+        public void TestDeposit()
+        {
+            bankAccount.Deposit(100m);
+
+            Assert.That(bankAccount.Balance, Is.EqualTo(200m), "Deposit");
+        }
+        [Test]
+        public void TestDepositWithNegativeSum()
+        {
+            Assert.That(()=>bankAccount.Deposit(-1000m), Throws.ArgumentException.With.Message.EqualTo("Sum must be positive number!"));
+        }
+        [Test]
+        public void TestWithdraw()
+        {
+            Assert.That(bankAccount.Withdraw(20m), Is.EqualTo(80m), "Withdraw");
+        }
+        [Test]
+        public void TestWithdrawMoreThanBalance()
+        {
+            Assert.That(() => bankAccount.Withdraw(500m), Throws.ArgumentException.With.Message.EqualTo("Balance can not be negative!"));
+        }
+    }
+}
---
+++ 21. UNIT TESTING/Unit/UnitTesting/BankAccount.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitTesting
+{
+    public class BankAccount
+    {
+        private decimal balance;
+        public BankAccount(decimal balance)
+        {
+            Balance = balance;
+        }
+        public decimal Balance
+        {
+            get => balance;
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Balance can not be negative!");
+                }
+
+                balance = value;
+            }
+        }
+
+        public void Deposit(decimal sum)
+        {
+            if(sum <= 0)
+            {
+                throw new ArgumentException("Sum must be positive number!");
+            }
+
+            Balance += sum;
+        }
+
+        public decimal Withdraw(decimal sum)
+        {
+            if (sum <= 0)
+            {
+                throw new ArgumentException("Sum must be positive number!");
+            }
+
+            Balance -= sum;
+
+            return Balance;
+        }
+
+
+    }
+}
